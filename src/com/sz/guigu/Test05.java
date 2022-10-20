2294fde607e6e066cd7c904f8f8293648cb8b97e
zwh <zwh@66.com>
2022-10-20 21:37:39 +0800

    hotfix a commit

--- src/com/sz/guigu/Test05.java
+++ src/com/sz/guigu/Test05.java
@@ -10,5 +10,6 @@ public class Test05 {
         System.out.println("测试1");
         System.out.println("测试2");
         System.out.println("hot-fix");
+        System.out.println("sdfsdfsdsdf");
     }
 }
